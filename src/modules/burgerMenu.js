4d74d722f3bc28f1a1cf0bc17997aa7b9c25653b
Trifle22 <xromchenkov99@gmail.com>
2021-03-23 03:54:38 +0300

    fix close-menu

--- src/modules/burgerMenu.js
+++ src/modules/burgerMenu.js
@@ -8,6 +8,7 @@ const burgerMenu = function() {
     menu.style.cssText = "display: flex; animation: fade 0.4s ease-in-out;";
 
     menu.addEventListener('click', function(event) {
+      const target = event.target;
       if (target.closest('.close-menu-btn')) {
         this.style.display = 'none';
       } else if (target.matches('a')) {
